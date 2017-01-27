76b3a478ba79c724781a150768ffc1eab2bb19be
Alan Nguyen <atngu101@syr.edu>
2017-01-27 11:11:53 -0500

    Completed Lesson 02

--- content/lessons/02/Now-You-Code/NYC4-Mad-Libs.py
+++ content/lessons/02/Now-You-Code/NYC4-Mad-Libs.py
@@ -1,13 +1,18 @@
-'''
-Now You Code 4: Mad-Libs!
+#Now You Code 4: Mad-Libs!
 
-Write a Python program which creates your own unique Mad-Libs! story.
+#Write a Python program which creates your own unique Mad-Libs! story.
 
-If you are not familar with a Mad-Libs! story, check out:
-http://www.madlibs.com and http://www.madtakes.com
+#If you are not familar with a Mad-Libs! story, check out: http://www.madlibs.com and http://www.madtakes.com
 
-Your story should take at least 5 inputs, and should include more than
-once sentence. 
-'''
+#Your story should take at least 5 inputs, and should include more than once sentence. 
+
+x= input('Part of the body: ')
+print("It doesn't matter how many layers I wear, my", x, "is always frozen in the winter!")
+y= input('Plural Noun: ')
+print("Your dirty socks smell like", y)
+z= input('Noun:')
+print('You look like you could use a/an', z)
+         
+
 
 
