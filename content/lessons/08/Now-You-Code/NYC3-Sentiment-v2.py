ef7ff6ef2325bddb59464b07221790e5228a7b88
Alan Nguyen <atngu101@syr.edu>
2017-03-16 17:50:26 -0500

    Finished HW08 2

--- content/lessons/08/Now-You-Code/NYC3-Sentiment-v2.py
+++ content/lessons/08/Now-You-Code/NYC3-Sentiment-v2.py
@@ -21,7 +21,35 @@ negative words to the text files!
 
 
 '''
-
+filename = "NYC3-pos.txt"
+filename1 = "NYC3-neg.txt"
 # TODO: Write Todo list then beneath write your code
-
+# Link the pos and neg .text
 # Write code here
+with open (filename,'r') as handle:
+    
+while True:
+    pos = load_words("NYC3-pos.txt")
+    neg = load_words("NYC3-neg.txt")
+    text = input('Enter Text: ')
+    if text == 'quit':
+        break
+        
+    def senti(pos, neg, text):
+        senti = 0
+        tokens = text.split(' ')
+        for word in tokens:
+            if word in pos:
+                senti = senti + 1
+            elif word in neg:
+                senti = senti - 1
+
+        if senti > 0:
+            print(senti, 'positive')
+        elif senti < 0:
+            print(senti, 'negative')
+        elif senti == 0:
+            print(senti, 'neutral')
+
+    senti(pos, neg, text)
+
